6880f9cbfe268894f3744e40068addc09fb6a185
Denis Homoki <homoki.denisth@gmail.com>
2022-08-20 14:43:50 +0300

    internship project started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Gallery from "./components/Gallery";
+import SearchBar from "./components/SearchBar";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar />
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Gallery() {
+  return (
+    <div>
+      <h1>This is the Gallery</h1>
+    </div>
+  );
+}
+
+export default Gallery;
